bc7787df79f1a2730cf9aa02d81e2e03ae68f45a
fukada <ps.ev.si.nc.mr.sw@gmail.com>
2019-08-10 16:46:37 +0900

    refactor

--- qiitascraper.py
+++ qiitascraper.py
@@ -2,8 +2,8 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-
 QIITA_URL = 'https://qiita.com/'
+
 
 def scrape_trend():
     response = requests.get(QIITA_URL)
@@ -12,23 +12,9 @@ def scrape_trend():
     return [item['node'] for item in json.loads(trend_text)['trend']['edges']]
 
 
-def post_slack_message(trend_items):
+def post_slack_message(items):
     webhook_url = ''
-
-    attachments = []
-    for index, item in enumerate(trend_items):
-        if index == 5:
-            break
-
-        attachment = {
-            'author_name': item['author']['urlName'],
-            'author_link': QIITA_URL + item['author']['urlName'],
-            'author_icon': item['author']['profileImageUrl'],
-            'title': item['title'],
-            'title_link': QIITA_URL + item['author']['urlName'] + '/items/' + item['uuid'],
-        }
-        attachments.append(attachment)
-
+    attachments = [_create_attachment(item) for item in items[:5]]
     post_data = {
         'username': 'trend_notifier',
         'text': 'Qiitaのトレンド記事をおしらせします',
@@ -36,6 +22,17 @@ def post_slack_message(trend_items):
     }
 
     requests.post(webhook_url, json.dumps(post_data), headers={'Content-Type': 'application/json'})
+
+
+def _create_attachment(item):
+    attachment = {
+        'author_name': item['author']['urlName'],
+        'author_link': QIITA_URL + item['author']['urlName'],
+        'author_icon': item['author']['profileImageUrl'],
+        'title': item['title'],
+        'title_link': QIITA_URL + item['author']['urlName'] + '/items/' + item['uuid'],
+    }
+    return attachment
 
 
 if __name__ == '__main__':
