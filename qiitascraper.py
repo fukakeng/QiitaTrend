88048c04d8b17eccf2b6cd3fdf897673905d74df
fukada <ps.ev.si.nc.mr.sw@gmail.com>
2020-10-09 19:07:50 +0900

    エラー時の通知を追加

--- qiitascraper.py
+++ qiitascraper.py
@@ -1,5 +1,6 @@
 import json
 import os
+import traceback
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,7 +15,7 @@ def scrape_trend():
     return [item['node'] for item in json.loads(trend_text)['trend']['edges']]
 
 
-def post_slack_message(items):
+def post_trend_message(items):
     webhook_urls = [v for k, v in os.environ.items() if 'webhook_url' in k]
     attachments = [_create_attachment(item) for item in items[:5]]
     post_data = {
@@ -38,6 +39,21 @@ def _create_attachment(item):
     return attachment
 
 
+def post_error_message(stack_trace):
+    post_data = {
+        'username': 'trend_notifier',
+        'text': 'トレンド記事の取得に失敗しました',
+        'attachments': [{'text': stack_trace, 'color': 'danger'}]
+    }
+
+    webhook_urls = [v for k, v in os.environ.items() if 'webhook_url' in k]
+    for webhook_url in webhook_urls:
+        requests.post(webhook_url, json.dumps(post_data), headers={'Content-Type': 'application/json'})
+
+
 if __name__ == '__main__':
-    trend_items = scrape_trend()
-    post_slack_message(trend_items)
+    try:
+        trend_items = scrape_trend()
+        post_trend_message(trend_items)
+    except AttributeError:
+        post_error_message(traceback.format_exc())
